d932dd2c6e51e5f8dd29c28b3a5d6bd5c1eaf5d6
James Aslett <jamesaslett1985@hotmail.com>
2023-12-29 19:25:45 +0000

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.VisualBasic;
-
+﻿
 namespace ConsoleApp1;
 
 internal class Program
@@ -45,44 +42,70 @@ internal class Program
 
     private static Player DetermineWinner(string[][] players)
     {
-        var one = players[0];
-        var two = players[1];
+        var playerOneCards = players[0];
+        var playerTwoCards = players[1];
 
-        //Suits: D/C/H/S
-        //Values (low > high): 2/3/4/5/6/7/8/9/10(T)J/Q/K/A
-        int p1Score = 0;
+        int playerOneScore = 0;
+        int playerTwoScore = 0;
 
-        int diamonds = one.SelectMany(x => x).Count(c => c == 'D');
-        int clubs = one.SelectMany(x => x).Count(c => c == 'C');
-        int hearts = one.SelectMany(x => x).Count(c => c == 'H');
-        int spades = one.SelectMany(x => x).Count(c => c == 'S');
+        int tens = playerOneCards.SelectMany(x => x).Count(c => c == 'T');
+        int jokers = playerOneCards.SelectMany(x => x).Count(c => c == 'J');
+        int queens = playerOneCards.SelectMany(x => x).Count(c => c == 'Q');
+        int kings = playerOneCards.SelectMany(x => x).Count(c => c == 'K');
+        int aces = playerOneCards.SelectMany(x => x).Count(c => c == 'A');
 
-        int tens = one.SelectMany(x => x).Count(c => c == 'T');
-        int jokers = one.SelectMany(x => x).Count(c => c == 'J');
-        int queens = one.SelectMany(x => x).Count(c => c == 'Q');
-        int kings = one.SelectMany(x => x).Count(c => c == 'K');
-        int aces = one.SelectMany(x => x).Count(c => c == 'A');
-
+        //royal flush
         if (tens > 0 && jokers > 0 && queens > 0 && kings > 0 && aces > 0)
         {
-            //royal flush
-            p1Score = 10;
+            playerOneScore = 10;
+        }
+        //straight flush - all cards in sequential order (asc/desc) (1st char) and same suit (2nd char)
+        if (1 == 1)
+        {
+            playerOneScore = 9;
+        }
+        //four of a kind - 4 cards are same value (1st char) 
+        if (1 == 1)
+        {
+            playerOneScore = 8;
+        }
+        //full house - three of a kind AND a pair
+        if (1 == 1)
+        {
+            playerOneScore = 7;
+        }
+        //flush - all cards are same suit (2nd char)
+        if (1 == 1)
+        {
+            playerOneScore = 6;
+        }
+        //straight - all cards in sequential order (asc/desc) (1st char)
+        if (1 == 1)
+        {
+            playerOneScore = 5;
+        }
+        //three of a kind - three cards of same value (1st char)
+        if (1 == 1)
+        {
+            playerOneScore = 5;
+        }
+        //two pairs - two different pairs (1st char)
+        if (1 == 1)
+        {
+            playerOneScore = 4;
+        }
+        //one pair - two cards of same value (1st char)
+        if (1 == 1)
+        {
+            playerOneScore = 3;
+        }
+        //high card - highest value card
+        if (1 == 1)
+        {
+            playerOneScore = 2; //whatever the highest value is
         }
 
-        //scores
-        //high card 1
-        //one pair 2
-        //two pairs 3
-        //three of a kind 4
-        //straight 5
-        //flush 6
-        //full house 7
-        //four of a kind 8
-        //straight flush 9
-
-        //once p1Score/p2Score is known, if they match then 
-
-        return (p1Score > 0) ? Player.PlayerOne : Player.PlayerTwo;
+        return (playerOneScore > 0) ? Player.PlayerOne : Player.PlayerTwo;
     }
 
     private enum Player
